73b47db2c2fae0719c4698393f530833a9069690
Andranik <bagdasarov14@yandex.ru>
2022-11-20 16:50:28 +0300

    Cheange commit

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,6 @@ public class Main {
         int ticket = 1000; // стоимость билета
         int mile = 20; // кол-во рублей для 1 бонусной мили
         int result = ticket / mile;
-        int bonus = 50; // кол-во бунысных миль
 
         System.out.println("Количество бонусных миль:" + result);
     }
